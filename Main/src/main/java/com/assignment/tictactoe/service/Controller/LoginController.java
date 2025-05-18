5e8e4e666568cd64f1b658487ce21785a468c881
DiilaNa <liyanaarachchidilan@gmail.com>
2025-05-18 18:55:46 +0530

    Login Controller added

---
+++ Main/src/main/java/com/assignment/tictactoe/service/Controller/LoginController.java
@@ -0,0 +1,21 @@
+package com.assignment.tictactoe.service.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private Button play;
+
+    @FXML
+    void playAction(ActionEvent event) {
+
+    }
+
+}
